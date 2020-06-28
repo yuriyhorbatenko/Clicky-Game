d8ce6c1363cfb3323dd7b2b3a1315f73a331ef6e
Yuriy <xett03@gmail.com>
2020-06-27 20:15:17 -0400

    heroku push

---
+++ build/precache-manifest.f530106c4a01de9ee78a1119ecd35a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3983f16b78e0568b996632268afa5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1e3252a2f99719fd06f",
+    "url": "/static/css/main.922b49bc.chunk.css"
+  },
+  {
+    "revision": "0cbedf5018513ea895fb",
+    "url": "/static/js/2.90f11f4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90f11f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e3252a2f99719fd06f",
+    "url": "/static/js/main.1d0597af.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
