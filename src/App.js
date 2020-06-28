1cd2bcb7c9c641507bfdb11e5908f09689854d83
Yuriy <xett03@gmail.com>
2020-06-28 01:45:57 -0400

    icons working good

--- src/App.js
+++ src/App.js
@@ -1,33 +1,72 @@
-import React from "react";
+import React, { Component } from 'react';
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import FriendCard from "./components/FriendCard";
-import friends from "./fighters.json";
+import Title from "./components/Header";
+import GameCards from "./components/GameCards";
+import fighters from "./fighters.json";
 
-function App() {
-  return (
-    <Wrapper>
-      <Title>Friends List</Title>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
-      />
-      <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
-      />
-      <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
-      />
-    </Wrapper>
-  );
+let Games = 0;
+let correctGuesses = 0;
+let bestScore = 0;
+let GameMessage = "Click on a Fighter Card to Start a Game!";
+
+class App extends Component {
+
+  state = {
+    fighters,
+    correctGuesses,
+    bestScore,
+    GameMessage
+  };
+
+  gameLogic = id => {
+
+    const fighters = this.state.fighters.filter(fighter => fighter.id === id);
+
+    if (fighters[0].clicked) {
+
+      correctGuesses = 0;
+      GameMessage = "You Already Clicked on this Fighter Card, Game Over!"
+
+      for (let i = 0; i < Games.length; i++) {
+        Games[i].clicked = false;
+      }
+
+      this.setState({ Games }, { correctGuesses }, { GameMessage });
+
+      console.log(GameMessage)
+      console.log("Games: " + Games)
+      console.log("Correct Guesses: " + correctGuesses)
+      console.log("Best Score: " + bestScore)
+
+    }
+
+  };
+
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.fighters.map(fighter => (
+          <GameCards
+            gameLogic={this.gameLogic}
+            id={fighter.id}
+            key={fighter.id}
+            name={fighter.name}
+            image={fighter.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+
+
+
+
 }
+
+
+
+
 
 export default App;
